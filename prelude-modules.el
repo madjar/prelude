9b92d8f38758680039cc5ae2d7797d6361d90be2
Georges Dubus <georges.dubus@compiletoi.net>
2014-12-05 14:44:44 +0100

    Updated prelude modules file

--- prelude-modules.el
+++ prelude-modules.el
@@ -4,6 +4,7 @@
 (require 'prelude-erc)
 (require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
 (require 'prelude-helm) ;; Interface for narrowing and search
+;; (require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
 ;; (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-mediawiki)
@@ -22,6 +23,7 @@
 (require 'prelude-js)
 ;; (require 'prelude-latex)
 (require 'prelude-lisp)
+;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 (require 'prelude-perl)
 (require 'prelude-python)
@@ -30,5 +32,6 @@
 (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
-(require 'prelude-web) ;; Emacs mode for web templates
+;; (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
+;; (require 'prelude-yaml)
