1541a23c8bc144df50cc57e0b60cc2b2eef9792a
Georges Dubus <georges.dubus@compiletoi.net>
2015-02-15 18:11:46 +0100

    Install nix-mode everywhere

--- personal/nix.el
+++ personal/nix.el
@@ -1,4 +1,1 @@
-(autoload 'nix-mode "nix-mode" "Major mode for editing Nix expressions." t)
-(push '("\\.nix\\'" . nix-mode) auto-mode-alist)
-(push '("\\.nix.in\\'" . nix-mode) auto-mode-alist)
-
+(prelude-require-package 'nix-mode)
